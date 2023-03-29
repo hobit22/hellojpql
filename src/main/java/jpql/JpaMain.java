482b9de08d9171a4bd9b3938ca929c7b3a8b0e60
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2023-03-30 00:27:49 +0900

    jpql 기본문법, 쿼리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,7 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,7 +16,15 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            Member singleResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+
+            System.out.println("singleResult = " + singleResult.getUsername());
+
 
             tx.commit();
         } catch (Exception e) {
@@ -33,3 +39,4 @@ public class JpaMain {
 
     }
 }
+
