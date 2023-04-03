4b43fe9ac3608c5281a43c45f3a2240c1baadbb5
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2023-04-03 23:51:22 +0900

    nullif

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -23,7 +23,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername(null);
+            member.setUsername("관리자");
             member.setAge(10);
             member.setTeam(team);
             member.setMemberType(MemberType.USER);
@@ -33,7 +33,8 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
+            String query = "select nullif(m.username, '관리자') as username " +
+                    "from Member m";
 
             List<String> resultList = em.createQuery(query, String.class).getResultList();
             for (String s : resultList) {
