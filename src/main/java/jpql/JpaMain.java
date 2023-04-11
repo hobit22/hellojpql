6c5db34e338979ef12e48df101077e57117ac6fc
hobit22 <hobit22@naver.com>
2023-04-11 23:58:56 +0900

    fetch join 한계 1. 컬랙션 fetch join 시 페이징 불가능 -> batch size 로 해결

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -43,11 +43,11 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t join fetch t.members";
+            String query = "select t From Team t";
 
             List<Team> resultList = em.createQuery(query, Team.class)
                     .setFirstResult(0)
-                    .setMaxResults(1)
+                    .setMaxResults(2)
                     .getResultList();
 
             for (Team team : resultList) {
