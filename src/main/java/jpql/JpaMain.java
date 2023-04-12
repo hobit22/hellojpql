50c3e946ab5aca644a749bacb7d65ec264ed958a
hobit22 <hobit22@naver.com>
2023-04-13 00:31:12 +0900

    벌크 연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -28,28 +28,30 @@ public class JpaMain {
             Member member1 = new Member();
             member1.setUsername("관리자1");
             member1.setTeam(teamA);
+            member1.setAge(0);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
             member2.setTeam(teamA);
+            member2.setAge(0);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("관리자3");
             member3.setTeam(teamB);
+            member3.setAge(0);
             em.persist(member3);
 
-            em.flush();
-            em.clear();
+            // flush
+            int resultCount = em.createQuery("update Member m set m.age = 20").executeUpdate();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            em.clear(); // 벌크 연산 후에는 전에 가져왔던 data 사용 안하는게 좋음
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            Member findMember = em.find(Member.class, member1.getId());
+
+            System.out.println("findMember = " + findMember);
+
 
             tx.commit();
         } catch (Exception e) {
